5c92f62a3db7369743e90f562348b5478c160f1d
rieznyk131 <m.rieznyk@gmail.com>
2026-03-11 16:50:09 +0100

    lesson-1/1.2 commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import {MyComponent} from "./components/MyComponent.tsx";
 
 function App() {
 
 
   return (
     <>
+
+      <MyComponent text={'Hello'}/>
+      <MyComponent text={'Goodbye'}/>
+
 
     </>
   )
