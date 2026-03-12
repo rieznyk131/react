574fd1520782f80b7c095fc8562d95616fd6109c
rieznyk131 <m.rieznyk@gmail.com>
2026-03-12 13:41:18 +0100

    homework-1/task-1 commit (coursesTitleArray)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import './App.css'
+import './App.css';
+import {coursesTitleArray} from './data/corsesTitleArray.ts';
+import type {CourseModelType} from './models/coursesTitleModel.ts';
+import CourseTitle from './components/coursesTitleComponent.tsx';
+import './components/coursesTitleComponent.css';
 
 function App() {
 
 
   return (
-    <>
+      <div className="container">
+        {coursesTitleArray.map((course:CourseModelType, index: number) => <CourseTitle key={index} course={course} /> )}
+      </div>
 
-    </>
   )
 }
 
---
+++ src/data/corsesTitleArray.ts
@@ -0,0 +1,10 @@
+import type {CourseModelType} from '../models/coursesTitleModel.ts'
+
+export const coursesTitleArray: CourseModelType[] = [
+    'JavaScript Complex',
+    'Java Complex',
+    'Python Complex',
+    'QA Complex',
+    'Fullstack',
+    'Frontend'
+];
