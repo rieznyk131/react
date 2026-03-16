d08017335ddf04eaef7acc105886a1e75f6fe10e
rieznyk131 <m.rieznyk@gmail.com>
2026-03-16 17:14:30 +0100

    lesson-2/state-lift

--- src/user-component/UserComponent.tsx
+++ src/user-component/UserComponent.tsx
@@ -3,31 +3,23 @@ import type {FC} from 'react'
 import './user-component-style.css'
 
 type UserPropsType = {
-    item: IUser
+    item: IUser,
+    details: (item: IUser) => void
 }
 
-export const UserComponent: FC<UserPropsType> = ({item}) => {
+export const UserComponent: FC<UserPropsType> = ({item, details}) => {
     return (
         <div className={'user-container'}>
             <div className={'avatar'}>
                 <img src="https://img.icons8.com/?size=100&id=iShwhFs38f4L&format=png&color=000000" alt={item.name}/>
             </div>
-            <div className={'user-info'}>
+            <h2 className='text-xl'>{item.name}</h2>
+            <button onClick={()=>{
+                console.log(item);
 
-                <h2 className={'text-xl font-bold pb-1'}>{item.name}</h2>
-                <p><span className='font-semibold'>Username</span>: {item.username}</p>
-                <p><span className='font-semibold'>E-mail</span>: <a href="mailto:{item.email}">{item.email}</a> </p>
-                <p><span className='font-semibold'>Phone</span>: {item.phone}</p>
-                <p><span className='font-semibold'>Web-site</span>: <a href="#">{item.website}</a></p>
-            </div>
-            <div className={'user-company-info'}>
-                <h3 className='text-xl font-bold pb-1'>Company:</h3>
-                <ul>
-                   <li><span className='font-semibold'>Name</span>: {item.company.name}</li>
-                   <li><span className='font-semibold'>Catch phrase</span>: {item.company.catchPhrase}</li>
-                   <li><span className='font-semibold'>Bs</span>: {item.company.bs}</li>
-                </ul>
-            </div>
+                details(item)
+            }}>Details</button>
+
          </div>
     )
 }
--- src/users-component/UsersComponent.tsx
+++ src/users-component/UsersComponent.tsx
@@ -2,22 +2,58 @@ import {useState, useEffect} from "react";
 import type {IUser} from '../models/IUser.ts'
 import {UserComponent} from '../user-component/UserComponent.tsx'
 import {getUsers} from "../services/api.service.ts";
+import './users-component-style.css'
 
 export const UsersComponent =() => {
     const [users, setUsers] = useState<IUser[]>([]);
+    const [item, setItems] = useState<IUser | null>(null);
+
+
     useEffect(() => {
         getUsers()
         .then(response => setUsers(response));
         return () => {
             console.log('done');
         }
-    },[])
+    },[]);
+
+    const details = (item: IUser) => {
+        setItems(item);
+    }
+
     return (
-        <div className='m-auto grid grid-cols-2 justify-items-center gap-y-20 w-3/4'>
+        <div >
             {
-                users.map(user => <UserComponent key={user.id} item={user}/>)
+                item && <div className={'selected-user-details'}>
+
+                    <div className={'user-info'}>
+                        <p><span className='font-semibold'>Username</span>: {item.username}</p>
+                        <p><span className='font-semibold'>E-mail</span>: <a href="mailto:{item.email}">{item.email}</a></p>
+                        <p><span className='font-semibold'>Phone</span>: {item.phone}</p>
+                        <p><span className='font-semibold'>Web-site</span>: <a href="#">{item.website}</a></p>
+                    </div>
+
+                <div className={'user-company-info'}>
+            <h3 className='text-l'>Company:</h3>
+            <ul>
+                <li><span className='font-semibold'>Name</span>: {item.company.name}</li>
+                <li><span className='font-semibold'>Catch phrase</span>: {item.company.catchPhrase}</li>
+                <li><span className='font-semibold'>Bs</span>: {item.company.bs}</li>
+            </ul>
+                </div>
+            </div>
             }
+
+            <div className={'users-container'}>
+                {
+                users.map(user => <UserComponent details={details} key={user.id} item={user}/>)
+                }
+            </div>
+
+
         </div>
+
+
     )}
 
 
