5b993f8303c8025c94e31a1f5056d3f5140033cf
rieznyk131 <m.rieznyk@gmail.com>
2026-03-26 15:23:59 +0100

    homework 2, task 1, part 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import {RouterProvider} from "react-router-dom";
 import {route} from "./router/Routes.tsx";
 
 createRoot(document.getElementById('root')!).render(
-    <RouterProvider router={route} />
+    <>
+        {route}
+    </>
 )
--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Layout} from "../layouts/Layout.tsx";
 import {HomePage} from "../pages/HomePage.tsx";
 import {ProductsPage} from "../pages/ProductsPage.tsx";
@@ -6,14 +6,17 @@ import {CommentsPage} from "../pages/CommentsPage.tsx";
 import {PostsPage} from "../pages/PostsPage.tsx";
 import {UsersPage} from "../pages/UsersPage.tsx";
 
-export const route = createBrowserRouter([
-    {
-        path:'', element: <Layout/>, children: [
-            {index: true, element: <HomePage/>},
-            {path: 'users', element: <UsersPage/>},
-            {path: 'posts', element: <PostsPage/>},
-            {path: 'comments', element: <CommentsPage/>},
-            {path: 'products', element: <ProductsPage/>},
-        ]
-    }
-])
+export const route =
+    <BrowserRouter>
+        <Routes>
+            <Route path="" element={<Layout/>}>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="users" element={<UsersPage/>}/>
+                <Route path="posts" element={<PostsPage/>}/>
+                <Route path="comments" element={<CommentsPage/>}/>
+                <Route path="products" element={<ProductsPage/>}/>
+            </Route>
+        </Routes>
+    </BrowserRouter>
+
+
